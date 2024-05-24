67ee32d87d288bb3346368933440a4817492e478
GajaSRajput <135072513+GajaSRajput@users.noreply.github.com>
2024-05-24 12:48:10 +0530

    Add files via upload

---
+++ bootcamp3/src/main/java/com/eduinx/bootcamp3/serviceimptn/UserServiceImptn.java
@@ -0,0 +1,12 @@
+package com.eduinx.bootcamp3.serviceimptn;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceImptn {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceImptn.class, args);
+	}
+
+}
